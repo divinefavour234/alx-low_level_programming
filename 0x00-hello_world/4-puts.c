8afe5c11061e29e20cfa04c52e50eb038f0f7c1c
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-08 13:20:40 +0100

    created a c code file that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* put print to the screen
+ * Return: Always 0 (Success)
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
