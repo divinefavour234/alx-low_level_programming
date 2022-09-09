544a151052be43a4526a3e9492295e36eee8d5f9
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-09 17:29:49 +0100

    Created a C code to run through and print numbers and alphabet in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print number base 16 which includes
+ * numbers and alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 48)
+	{
+		if (i < 10)
+			putchar(i + '0');
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+		i++;
+	}
+
+	putchar (10);
+	return (0);
+}
