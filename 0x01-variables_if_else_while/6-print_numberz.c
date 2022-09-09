354e1351c5d4a5a1c20821bb392145bd7091854d
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-09 16:33:10 +0100

    Create C code and complied it to print out list of number from 0 to base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print number base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
