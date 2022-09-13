75656f4ed781ca672613814f4106e5d07fe8e435
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-13 11:13:59 +0100

    Added main.h file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Description: Contains declaration of all function 
+ * used in 0x02-functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+#endif
