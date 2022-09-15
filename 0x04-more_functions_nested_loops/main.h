b507050b65f1e7c862e62163b6522fc0e285e450
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-15 09:53:05 +0100

    Wrote a function to check digits

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,11 +2,11 @@
 #define MAIN_H
 
 /**
- * File: main.h
  *
  * Description: Contains declaration of all function
  * used in 0x02-functions_nested_loops directory
  */
 
 int _isupper(int c);
+int _isdigit(int c);
 #endif
