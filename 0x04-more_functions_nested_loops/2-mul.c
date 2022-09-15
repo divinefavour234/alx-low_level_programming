7f03c41dbcabfc19f0623eb393b7e3689cbf5632
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-15 10:01:40 +0100

    Wrote a funtion that multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - Declaration of Function mul
+ *
+ * Description: Multiply two numbers together
+ *
+ * @a: Accept parameter which should be an integer
+ *
+ * @b: Accept parameter which should be an integer
+ *
+ * Return: return (c) which will be the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
