5377af9428129662a659de7f0a688b093a556bb0
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-19 16:55:27 +0100

    Wrote a function that print a string followed by a new line

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void _putchar(char c);
+int _putchar(char c);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
 int _strlen(char *s);
+void _puts(char *str);
 #endif /*MAIN_H*/
