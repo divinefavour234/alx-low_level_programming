afd4451751ac409e3453afe4a13e5ff9296a3642
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-09-26 11:15:44 +0100

    Create a function that fills memory with a constant byte and created the main.h file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - function fills the first n bytes of the memory
+ * @s: An array of characters
+ * @b: a character to be replac the array of characters
+ * @n: number of bytes of the memory
+ * Return s: Return the array
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
