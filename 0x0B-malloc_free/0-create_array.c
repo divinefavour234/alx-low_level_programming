be08853d4a3d7242245499a9f28490d82560991b
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-10-03 22:41:47 +0100

    Wrote a function that creates an array of chars, and initializes it with a specific char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_array - Creates an array
+ * @size: Tell the size of the array
+ * @c: the character that each value of array must hold
+ * Return: Return th buffer which is the
+ * value at the memory allocated
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char * buffer;
+	unsigned int positioncount;
+
+	if(size == 0;)
+	{
+		return(0);
+	}
+	buffer = malloc(sizeof(char) * size);
+
+	if(buffer == NULL)
+	{
+		return (NULL);
+	}
+	for (positioncount = 0; positioncount < size; positioncount++)
+	{
+		buffer[positioncount] = c;
+	}
+	return (buffer);
+}
