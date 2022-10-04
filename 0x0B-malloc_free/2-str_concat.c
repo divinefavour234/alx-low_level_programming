e3a020f9c9a9b66ce1c23ca865863d49d87e41d4
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-10-04 22:43:22 +0100

    correct function that concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,9 +20,9 @@ char *str_concat(char *s1, char *s2)
 	int c2 = 0;
 
 	if (s1 == NULL)
-		s1 == '\0';
+		s1[0] = '\0';
 	if (s2 == NULL)
-		s2 == '\0';
+		s2[0] = '\0';
 	while (s1[c1] != '\0')
 	{
 		c1++;
