9c58ecb142a5fbb78a5b246c07ab43c37ea1bf83
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-10-04 18:49:30 +0100

    create a function that copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strdup - Copy a string given as a parameter
+ *
+ * @str: string to be enter
+ *
+ * Return: NULL  if the parameter is NULL if memory
+ * is not allocated. else return the a pointer
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	int count = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[count] != 0)
+	{
+		count++;
+	}
+	ptr = malloc((sizeof(char) * count) + 1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (int i = 0; i < count; i++)
+	{
+		ptr[i] = str[i];
+	}
+	return (ptr);
+}
