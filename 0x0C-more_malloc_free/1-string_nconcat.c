442ad579c35340ff947c49d95cce2ac0124e50a8
Divine-favour <divinefavourdamilare2019@gmail.com>
2022-10-07 01:28:10 +0100

    Done with all tasks onece

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,52 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * string_nconcat - Concatenate two string
- *
- * @s1: the first string
- * @s2: the second string
- * @n: the number of bytes of s2 that should be concatenated
- *
- * Return: NULl if s1 or s2 is null, return pointer
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+
+unsigned int _strlen(char *s)
+{
+unsigned int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: first bytes of s2 to be used
+ * Return: pointer or NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	unsigned int s1lng = 0;
-	unsigned int s2lng = 0;
-	int j = 0;
-	unsigned int i = 0;
-	unsigned int strlng;
+unsigned int i, j;
+char *m;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s1 = "";
-	while (s1[s1lng] != '\0')
-		s1lng++;
-	while (s2[s2lng] != '\0')
-		s2lng++;
-	if (n >= s2lng)
-		strlng = s1lng + s2lng;
-	else
-		strlng = s1lng + n;
-	ptr = malloc((sizeof(char)) * strlng + 1);
-	if (ptr == NULL)
-		return (NULL);
-	while (i < s1lng)
-	{
-		ptr[i] = s1[i];
-		i++;
-	}
-	while (s1lng < strlng)
-	{
-		ptr[s1lng] = s2[j];
-		j++;
-		s1lng++;
-	}
-	return (ptr);
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+
+if (n < _strlen(s2))
+	m = malloc(_strlen(s1) + n * sizeof(char) + 1);
+else
+	m = malloc(_strlen(s1) + _strlen(s2) + 1);
+
+if (m == 0)
+	return (NULL);
+
+for (i = 0; s1[i] != '\0'; i++)
+	m[i] = s1[i];
+
+for (j = 0; s2[j] != '\0' && j < n; i++, j++)
+	m[i] = s2[j];
+
+m[i] = '\0';
+
+return (m);
 }
